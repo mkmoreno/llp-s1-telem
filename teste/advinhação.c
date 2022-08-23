21913a16acf697f473d60983bd7363c42cc734d1
Mikael S S Moreno <111381720+mkmoreno@users.noreply.github.com>
2022-08-23 11:27:57 -0300

    Correção do Programa

--- teste/advinhação.c
+++ teste/advinhação.c
@@ -4,26 +4,25 @@
 int main () {
 	setlocale (LC_ALL, "Portuguese"); 
 	printf ("***********************************\n");
-	printf ("* Bem-vindo ao jogo da Advinhação *\n");
+	printf ("* Bem-vindo ao Jogo da Advinhação *\n");
 	printf ("***********************************\n");
 	
-    int numerosecreto;
-	int chute;
+  int numerosecreto;
 	numerosecreto = 42;
-	int acertou = chute == numerosecreto;
-	int maior = chute > numerosecreto;
-	int menor = chute < numerosecreto;
-	
+
+  int chute;
 	printf ("Qual é o seu chute: ");
 	scanf ("%d", &chute);
 	printf ("Você chutou o numero: %d!\n", chute);
-	
+	int acertou = chute == numerosecreto;
 	if (acertou) {
 		printf ("Parabéns! Você acertou!");
 	} else {
+    int maior = chute > numerosecreto;
 		if (maior) {
 			printf ("Seu chute foi maior que o número secreto!\n");
 		}
+    int menor = chute < numerosecreto;
 		if(menor) {
 		printf ("Seu chute foi menor que o número secreto!");
 		}
