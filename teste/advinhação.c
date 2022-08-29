0b3dfa504398e6e3debf1b03a0b3c242390b29aa
Mikael S S Moreno <111381720+mkmoreno@users.noreply.github.com>
2022-08-29 12:21:52 -0300

    Alterado em 29-08-2022

--- teste/advinhação.c
+++ teste/advinhação.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <locale.h>
+#define NUMERO_DE_TENTATIVAS 3
 
 int main () {
 	setlocale (LC_ALL, "Portuguese"); 
 	printf ("***********************************\n");
-	printf ("* Bem-vindo ao Jogo da Advinhação *\n");
+	printf ("* Bem-vindo ao Jogo da AdvinhaÃ§Ã£o *\n");
 	printf ("***********************************\n");
 	
   int numerosecreto;
 	numerosecreto = 42;
-
   int chute;
-	printf ("Qual é o seu chute: ");
-	scanf ("%d", &chute);
-	printf ("Você chutou o numero: %d!\n", chute);
+  for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++){
+  	printf ("\nQual é o seu %d° chute: ",i);
+	  scanf ("%d", &chute);
 	int acertou = chute == numerosecreto;
 	if (acertou) {
 		printf ("Parabéns! Você acertou!");
+    printf("\nObrigado por jogar!\n");
+    break;
 	} else {
     int maior = chute > numerosecreto;
 		if (maior) {
-			printf ("Seu chute foi maior que o número secreto!\n");
+		printf ("Seu chute foi maior que o número secreto!");
 		}
     int menor = chute < numerosecreto;
 		if(menor) {
 		printf ("Seu chute foi menor que o número secreto!");
 		}
-	}
-}
+	 }
+  }
+  printf("\nFim de Jogo!\n");
+ }
